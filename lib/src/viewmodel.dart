9d894efea4250ce2f184014b85a636c0de47fa8c
Stephan E.G. Veenstra <sveenstra@newnexusmobile.nl>
2020-11-01 20:38:13 +0100

    Initial commit

---
+++ lib/src/viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+
+abstract class ViewModel extends InheritedWidget {
+  ViewModel({@required Widget child, Key key})
+      : super(child: child, key: key) {}
+
+  ViewModel.withDependencies(BuildContext context,
+      {@required Widget child, Key key})
+      : super(child: child, key: key);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return false;
+  }
+
+  static T of<T extends ViewModel>(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<T>();
+  }
+}
