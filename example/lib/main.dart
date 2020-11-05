3a7f37295740e737357c8fe512fca2699a59c2a4
Stephan E.G. Veenstra <sveenstra@newnexusmobile.nl>
2020-11-05 08:25:38 +0100

    Changed the sample to a simple color changing app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,144 +2,42 @@ import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:viewmodel/viewmodel.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class ChartViewModel extends ViewModel {
+class ColorViewModel extends ViewModel {
   final _rng = Random();
 
-  final ValueNotifier<List<ChartData>> _salesData = ValueNotifier([
-    ChartData(2018, 35),
-    ChartData(2019, 28),
-    ChartData(2020, 34),
-  ]);
-  ValueListenable<List<ChartData>> get chartData => _salesData;
+  final ValueNotifier<Color> _color = ValueNotifier(Colors.blue);
+  ValueListenable<Color> get color => _color;
 
-  ChartViewModel({Widget child}) : super(child: child);
+  ColorViewModel({Widget child}) : super(child: child) {
+    refreshData();
+  }
 
   refreshData() {
-    _salesData.value = [
-      ChartData(2018, _rng.nextDouble() * 10),
-      ChartData(2019, _rng.nextDouble() * 10),
-      ChartData(2020, _rng.nextDouble() * 10),
-    ];
+    _color.value = Color(_rng.nextInt(0xffffff)).withAlpha(255);
   }
 }
 
-class ChartData {
-  ChartData(this.year, this.values);
-  final int year;
-  final double values;
-}
-
-class PieChart extends ViewModelConsumer<ChartViewModel> {
+class ColorWidget extends ViewModelConsumer<ColorViewModel> {
   @override
-  Widget buildView(BuildContext context, ChartViewModel viewModel) {
-    return Stack(
-      children: [
-        ValueListenableBuilder<List<ChartData>>(
-            valueListenable: viewModel.chartData,
-            builder: (context, data, _) {
-              return SfCircularChart(
-                series: [
-                  PieSeries<ChartData, int>(
-                      dataSource: data,
-                      xValueMapper: (ChartData data, _) => data.year,
-                      yValueMapper: (ChartData data, _) => data.values)
-                ],
-              );
-            }),
-        InkWell(
-          onTap: () {
-            viewModel.refreshData();
-          },
-        )
-      ],
-    );
-  }
-}
-
-class DoughnutChart extends ViewModelConsumer<ChartViewModel> {
-  @override
-  Widget buildView(BuildContext context, ChartViewModel viewModel) {
-    return Stack(
-      children: [
-        ValueListenableBuilder<List<ChartData>>(
-            valueListenable: viewModel.chartData,
-            builder: (context, data, _) {
-              return SfCircularChart(
-                series: [
-                  DoughnutSeries<ChartData, int>(
-                      dataSource: data,
-                      xValueMapper: (ChartData data, _) => data.year,
-                      yValueMapper: (ChartData data, _) => data.values)
-                ],
-              );
-            }),
-        InkWell(
-          onTap: () {
-            viewModel.refreshData();
-          },
-        )
-      ],
-    );
-  }
-}
-
-class LineChart extends ViewModelConsumer<ChartViewModel> {
-  @override
-  Widget buildView(BuildContext context, ChartViewModel viewModel) {
-    return Stack(
-      children: [
-        ValueListenableBuilder<List<ChartData>>(
-            valueListenable: viewModel.chartData,
-            builder: (context, data, _) {
-              return SfCartesianChart(
-                series: [
-                  AreaSeries<ChartData, int>(
-                      dataSource: data,
-                      xValueMapper: (ChartData data, _) => data.year,
-                      yValueMapper: (ChartData data, _) => data.values)
-                ],
-              );
-            }),
-        InkWell(
-          onTap: () {
-            viewModel.refreshData();
-          },
-        )
-      ],
-    );
-  }
-}
-
-class ColumnChart extends ViewModelConsumer<ChartViewModel> {
-  @override
-  Widget buildView(BuildContext context, ChartViewModel viewModel) {
-    return Stack(
-      children: [
-        ValueListenableBuilder<List<ChartData>>(
-            valueListenable: viewModel.chartData,
-            builder: (context, data, _) {
-              return SfCartesianChart(
-                series: [
-                  ColumnSeries<ChartData, int>(
-                      dataSource: data,
-                      xValueMapper: (ChartData data, _) => data.year,
-                      yValueMapper: (ChartData data, _) => data.values)
-                ],
-              );
-            }),
-        InkWell(
-          onTap: () {
-            viewModel.refreshData();
-          },
-        )
-      ],
+  Widget buildView(BuildContext context, ColorViewModel viewModel) {
+    return InkWell(
+      onTap: () {
+        viewModel.refreshData();
+      },
+      child: ValueListenableBuilder<Color>(
+          valueListenable: viewModel.color,
+          builder: (context, color, _) {
+            return AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              color: color,
+            );
+          }),
     );
   }
 }
@@ -157,21 +55,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('ViewModel Sample'),
         ),
-        body: ChartViewModel(
+        body: ColorViewModel(
           child: Column(mainAxisSize: MainAxisSize.max, children: [
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: LineChart()),
-                  Expanded(child: DoughnutChart()),
+                  Expanded(child: ColorWidget()),
+                  Expanded(child: ColorViewModel(child: ColorWidget())),
                 ],
               ),
             ),
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: PieChart()),
-                  Expanded(child: ColumnChart()),
+                  Expanded(child: ColorWidget()),
                 ],
               ),
             )
